a9b225ea4ce3242a35e34830f0582ac6aa3c68e0
Thenx0009 <ayushverm0009@gmail.com>
2024-03-23 21:59:16 +0530

    08-spring-boot-mvc-security

---
+++ 08-spring-boot-mvc-security/spring-boot-spring-mvc-security-user-registration/src/main/java/com/example/demo/SpringBootSpringMvcSecurityUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringMvcSecurityUserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringMvcSecurityUserRegistrationApplication.class, args);
+	}
+
+}
